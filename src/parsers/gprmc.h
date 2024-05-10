163c57d7cf415bd0a038ccb98c39dcfc11528240
Lucas Zembrani Folle <lucas.zfolle@gmail.com>
2024-05-09 21:16:37 -0300

    Refactored RMC parser

--- src/parsers/gprmc.h
+++ src/parsers/gprmc.h
@@ -4,34 +4,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <time.h>
 #include <nmea.h>
 
-typedef struct {
+typedef struct
+{
 	nmea_s base;
-	struct tm date_time;
-	nmea_position longitude;
-	nmea_position latitude;
-	double gndspd_knots;
-	double track_deg;
-	double magvar_deg;
-	nmea_cardinal_t magvar_cardinal;
-	//The direction of the magnetic variation determines whether or not it
-	//is additive - Easterly means subtract magvar_deg from track_deg and
-	//westerly means add magvar_deg to track_deg for the correct course.
+	nmea_time_date_t date_time;
+	double latitude;
+	double longitude;
+	float gndspd_knots;
+	float track_deg;
+	// The direction of the magnetic variation determines whether or not it
+	// is additive - Easterly means subtract magvar_deg from track_deg and
+	// westerly means add magvar_deg to track_deg for the correct course.
+	float magvar_deg;
 	bool valid;
 } nmea_gprmc_s;
 
 /* Value indexes */
-#define NMEA_GPRMC_TIME			0
-#define NMEA_GPRMC_STATUS		1
-#define NMEA_GPRMC_LATITUDE		2
-#define NMEA_GPRMC_LATITUDE_CARDINAL	3
-#define NMEA_GPRMC_LONGITUDE		4
-#define NMEA_GPRMC_LONGITUDE_CARDINAL	5
-#define NMEA_GPRMC_GNDSPD_KNOTS		6
-#define NMEA_GPRMC_TRUECOURSE_DEG	7
-#define NMEA_GPRMC_DATE			8
-#define NMEA_GPRMC_MAGVAR_DEG		9
-#define NMEA_GPRMC_MAGVAR_CARDINAL	10
-#endif  /* INC_NMEA_GPRMC_H */
+enum
+{
+	NMEA_GPRMC_TIME,
+	NMEA_GPRMC_STATUS,
+	NMEA_GPRMC_LATITUDE,
+	NMEA_GPRMC_LATITUDE_CARDINAL,
+	NMEA_GPRMC_LONGITUDE,
+	NMEA_GPRMC_LONGITUDE_CARDINAL,
+	NMEA_GPRMC_GNDSPD_KNOTS,
+	NMEA_GPRMC_TRUECOURSE_DEG,
+	NMEA_GPRMC_DATE,
+	NMEA_GPRMC_MAGVAR_DEG,
+	NMEA_GPRMC_MAGVAR_CARDINAL
+};
+
+#endif /* INC_NMEA_GPRMC_H */
